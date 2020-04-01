70819b993bf98e29611b99bbf74495f9595bf4e1
Heather Moreira <moreiraheather@gmail.com>
2020-04-01 22:15:01 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -9,7 +9,7 @@ def prime?(integer)
   denominators = (2..integer - 1).to_a 
   if integer < 2
     false 
-  else 
+  end 
     !(denominators.any? do |denominator| 
       integer % denominator == 0
       end)
